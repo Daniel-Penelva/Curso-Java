0ba6b75d2ad50ca78127eb67100055cb1576c5c4
Daniel Penelva <d4n.andrade@gmail.com>
2022-09-20 02:00:46 -0300

    cálculo de soma

--- primeiroProgramaJava/src/curso/executavel/PrimeiroProgramaJava.java
+++ primeiroProgramaJava/src/curso/executavel/PrimeiroProgramaJava.java
@@ -5,5 +5,9 @@ public class PrimeiroProgramaJava {
 	public static void main(String[] args) {
 		System.out.println("Olá Mundo!");
 		System.out.println("Testando github");
+		
+		int numero1 = 10, numero2 = 20, soma;
+		soma = numero1 + numero2;
+	    System.out.println("Soma" + soma);
 	}
 }
