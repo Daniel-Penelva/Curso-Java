f23a1578d6301a72ec0e6e6fd5f8c0a9c4c12d13
Daniel Penelva <d4n.andrade@gmail.com>
2022-09-20 19:42:02 -0300

    Alterando codigo e exemplificando branch

--- primeiroProgramaJava/src/curso/executavel/PrimeiroProgramaJava.java
+++ primeiroProgramaJava/src/curso/executavel/PrimeiroProgramaJava.java
@@ -6,12 +6,15 @@ public class PrimeiroProgramaJava {
 		System.out.println("Olá Mundo!");
 		System.out.println("Testando github");
 		
-		int numero1 = 10, numero2 = 20, soma, subtracao;
+		int numero1 = 10, numero2 = 20, soma, subtracao, multiplicacao;
 		soma = numero1 + numero2;
-	    System.out.println("Soma" + soma);
+	    System.out.println("Soma: " + soma);
 	    
 	    subtracao = numero1 - numero2;
 	    
 	    System.out.println("Subtração: " + subtracao);
+	    
+	    multiplicacao = numero1 * numero2;
+	    System.out.println("Multiplicação: " + multiplicacao);
 	}
 }
