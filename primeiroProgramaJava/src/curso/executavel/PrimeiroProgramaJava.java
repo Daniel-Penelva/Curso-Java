9a6885c4a0589507d336a08a52c2597cf0a8fbf3
Daniel Penelva <d4n.andrade@gmail.com>
2022-09-20 02:25:21 -0300

    adicionando calculo de subtração

--- primeiroProgramaJava/src/curso/executavel/PrimeiroProgramaJava.java
+++ primeiroProgramaJava/src/curso/executavel/PrimeiroProgramaJava.java
@@ -6,8 +6,12 @@ public class PrimeiroProgramaJava {
 		System.out.println("Olá Mundo!");
 		System.out.println("Testando github");
 		
-		int numero1 = 10, numero2 = 20, soma;
+		int numero1 = 10, numero2 = 20, soma, subtracao;
 		soma = numero1 + numero2;
 	    System.out.println("Soma" + soma);
+	    
+	    subtracao = numero1 - numero2;
+	    
+	    System.out.println("Subtração: " + subtracao);
 	}
 }
