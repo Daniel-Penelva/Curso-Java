f9f0993695035918e4153ea059430014f890adf4
Daniel Penelva <d4n.andrade@gmail.com>
2022-09-20 19:57:40 -0300

    criando divisão e exemplificando outra branch

--- primeiroProgramaJava/src/curso/executavel/PrimeiroProgramaJava.java
+++ primeiroProgramaJava/src/curso/executavel/PrimeiroProgramaJava.java
@@ -6,12 +6,15 @@ public class PrimeiroProgramaJava {
 		System.out.println("Olá Mundo!");
 		System.out.println("Testando github");
 		
-		int numero1 = 10, numero2 = 20, soma, subtracao;
+		int numero1 = 10, numero2 = 2, soma, subtracao, divisao;
 		soma = numero1 + numero2;
 	    System.out.println("Soma" + soma);
 	    
 	    subtracao = numero1 - numero2;
 	    
 	    System.out.println("Subtração: " + subtracao);
+	    
+	    divisao = numero1/numero2;
+	    System.out.println("Divisão: " + divisao);
 	}
 }
